9d9a3b23c184337f158e123feb0bd088607c3d7f
Hripsime <hripsime.martirosyan@shortcut.no>
2022-02-01 14:13:31 +0100

    Added goal and path entities for pathfinder

---
+++ Sources/VSPositionKit/VPS/Pathfinder/Path.swift
@@ -0,0 +1,15 @@
+//
+//  Path.swift
+//  PositionKit
+//
+//  Created by Hripsime on 2022-01-31.
+//
+
+import Foundation
+import CoreGraphics
+
+public struct Path {
+    public let head: [CGPoint]
+    public let body: [CGPoint]
+    public let tail: [CGPoint]
+}
