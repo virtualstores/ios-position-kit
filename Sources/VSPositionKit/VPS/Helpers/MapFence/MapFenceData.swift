aad3364bfdf03d16e429d89126883b330f5ac93b
Hripsime <hripsime.martirosyan@shortcut.no>
2022-02-04 10:34:31 +0100

    Added MapFenceData

---
+++ Sources/VSPositionKit/VPS/Helpers/MapFence/MapFenceData.swift
@@ -0,0 +1,38 @@
+//
+// MapFenceData
+// VSPositionKit
+//
+// Created by Hripsime on 2022-02-04
+// Copyright Virtual Stores - 2022
+//
+
+import Foundation
+import UIKit
+import vps
+
+public struct MapFenceData {
+    private let flippedYAxis: Bool
+    let width: Int
+    let height: Int
+    let widthInPixels: Double
+    let heightInPixels: Double
+    private var _polygons: [[PointF]]
+
+    var polygons: [[PointF]] {
+        get {
+            return flippedYAxis ? _polygons.map { $0.map { PointF(x: Double($0.x), y: self.heightInPixels - Double($0.y)) }} : _polygons
+        }
+        set {
+            self._polygons = newValue
+        }
+    }
+
+    public init (width: Int, height: Int, widthInPixels: Double, heightInPixels: Double, polygons: [[PointF]], flippedYAxis: Bool = true) {
+        self.width = width
+        self.height = height
+        self.widthInPixels = widthInPixels
+        self.heightInPixels = heightInPixels
+        self.flippedYAxis = flippedYAxis
+        self._polygons = polygons
+    }
+}
