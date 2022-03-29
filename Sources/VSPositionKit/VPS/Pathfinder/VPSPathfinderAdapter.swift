4b377e8f3fb4ad4735ad9b93566f3c4d5a091458
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2022-03-29 17:26:11 +0200

    Stuff for pathfinder

--- Sources/VSPositionKit/VPS/Pathfinder/VPSPathfinderAdapter.swift
+++ Sources/VSPositionKit/VPS/Pathfinder/VPSPathfinderAdapter.swift
@@ -20,14 +20,20 @@ public final class VPSPathfinderAdapter: IFoundationPathfinder {
     }
     public var pathUpdatedPublisher: CurrentValueSubject<Path?, Never> = .init(nil)
 
-    public  var hasGoal: CurrentValueSubject<Bool, Never> {
+    public var hasGoal: CurrentValueSubject<Bool, Never> {
         .init(self.vpsPathfinder.hasGoal)
     }
 
     private let vpsPathfinder: IPathfinder
+    private let converter: ICoordinateConverter
+    private let height: Double
 
-    public init(heightInPixels: Float, widthInPixels: Float, pixelsPerMeter: Float, navGraph: NavGraph, startPosition: CGPoint, stopPosition: CGPoint, pathRefreshDistance: Float = 100.0) {
-        self.vpsPathfinder = BasePathfinder(heightInPixels: heightInPixels, widthInPixels: widthInPixels, pixelsPerMeter: pixelsPerMeter, navGraph: navGraph, pathRefreshDistance: pathRefreshDistance, startPosition: startPosition.asPointF, stopPosition: stopPosition.asPointF)
+    public init(converter: ICoordinateConverter, height: Double, width: Double, pixelsPerMeter: Float, navGraph: TT2NavGraph, startPosition: CGPoint, stopPosition: CGPoint, pathRefreshDistance: Float = 100.0) {
+        self.converter = converter
+        self.height = height
+        let heightInPixels = converter.convertFromMetersToPixels(input: height)
+        let widthInPixels = converter.convertFromMetersToPixels(input: width)
+        self.vpsPathfinder = BasePathfinder(heightInPixels: Float(heightInPixels), widthInPixels: Float(widthInPixels), pixelsPerMeter: pixelsPerMeter, navGraph: navGraph.asNavGraph, pathRefreshDistance: pathRefreshDistance, startPosition: startPosition.asPointF, stopPosition: stopPosition.asPointF)
 
         self.vpsPathfinder.addListener(listener_: self)
     }
@@ -37,7 +43,7 @@ public final class VPSPathfinderAdapter: IFoundationPathfinder {
     }
 
     public func add(goal: Goal, completion: @escaping () -> Void) {
-        self.vpsPathfinder.addGoal(goal: goal.asVPSGoal, callback: completion)
+        self.vpsPathfinder.addGoal(goal: goal.convertFromMeterToPixel(converter: converter, height: height).asVPSGoal, callback: completion)
     }
 
     public func add(goals: [Goal], completion: @escaping () -> Void) {
@@ -67,8 +73,6 @@ public final class VPSPathfinderAdapter: IFoundationPathfinder {
 
 extension VPSPathfinderAdapter: IPathfinderListener {
     public var id_: String { "VPSPathfinderAdapter" }
-    
-    public var id: String { "VPSPathfinderAdapter" }
 
     public func onCurrentGoalUpdated(goal: IPathfinderGoal?) {
         currentGoalUpdatedPublisher.send( goal?.asGoal)
@@ -80,5 +84,43 @@ extension VPSPathfinderAdapter: IPathfinderListener {
 
     public func onSortedGoalUpdated(goals: [IPathfinderGoal]) {
         sortedGoalUpdatedPublisher.send( goals.map { $0.asGoal })
+    }
+}
+
+extension TT2NavGraph {
+  var asNavGraph: NavGraph {
+    var modifiedVertices: [String : NavGraph.Vertex] = [:]
+    vertices.forEach { (key, value) in
+      var neighborCosts: [String : KotlinInt] = [:]
+      value.neighborCosts.forEach { (key, value) in
+        neighborCosts[key] = KotlinInt(int: value)
+      }
+      modifiedVertices[key] = NavGraph.Vertex(id: value.id, x: value.x, y: value.y, cost: value.cost, neighborCosts: neighborCosts)
+    }
+    return NavGraph(spacing: Int32(spacing), numVertices: numVertices, vertices: modifiedVertices)
+  }
+}
+
+extension Goal {
+    func convertFromMeterToPixel(converter: ICoordinateConverter, height: Double = 0.0) -> Goal {
+        let x = converter.convertFromMetersToPixels(input: position.x)
+        let y: Double
+        if height > 0.0 {
+            y = converter.convertFromMetersToPixels(input: height - position.y)
+        } else {
+            y = converter.convertFromMetersToPixels(input: position.y)
+        }
+        return Goal(id: id, position: CGPoint(x: x, y: y), data: data, type: type)
+    }
+
+    func convertFromPixelToMeter(converter: ICoordinateConverter, height: Double = 0.0) -> Goal {
+        let x = converter.convertFromPixelsToMeters(input: position.x)
+        let y: Double
+        if height > 0.0 {
+            y = converter.convertFromPixelsToMeters(input: height - position.y)
+        } else {
+            y = converter.convertFromPixelsToMeters(input: position.y)
+        }
+        return Goal(id: id, position: CGPoint(x: x, y: y), data: data, type: type)
     }
 }
