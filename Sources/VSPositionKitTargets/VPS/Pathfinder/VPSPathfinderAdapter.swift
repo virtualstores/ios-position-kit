73ecd40c4471ef935d4db56c51b32ebab9477b9e
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2022-04-25 09:48:53 +0200

    Moved calls to VPSPathinder to background thread

--- Sources/VSPositionKitTargets/VPS/Pathfinder/VPSPathfinderAdapter.swift
+++ Sources/VSPositionKitTargets/VPS/Pathfinder/VPSPathfinderAdapter.swift
@@ -21,6 +21,7 @@ public final class VPSPathfinderAdapter: IFoundationPathfinder {
     }
 
     private let vpsPathfinder: IPathfinder
+    let serialDispatch: DispatchQueue = DispatchQueue(label: "se.tt2.pathfinder")
     private let converter: ICoordinateConverter
     private let height: Double
 
@@ -35,35 +36,35 @@ public final class VPSPathfinderAdapter: IFoundationPathfinder {
     }
 
     public func setUserPosition(position: CGPoint?) {
-        self.vpsPathfinder.setUserPosition(position: position?.asPointF)
+        serialDispatch.async { self.vpsPathfinder.setUserPosition(position: position?.asPointF) }
     }
 
     public func add(goal: Goal, completion: @escaping () -> Void) {
-        self.vpsPathfinder.addGoal(goal: goal.asVPSGoal, callback: completion)
+        serialDispatch.async { self.vpsPathfinder.addGoal(goal: goal.asVPSGoal, callback: completion) }
     }
 
     public func add(goals: [Goal], completion: @escaping () -> Void) {
-        self.vpsPathfinder.addGoals(goals: goals.map { $0.asVPSGoal }, callback: completion)
+        serialDispatch.async { self.vpsPathfinder.addGoals(goals: goals.map { $0.asVPSGoal }, callback: completion) }
     }
 
     public func set(goals: [Goal], completion: @escaping () -> Void) {
-        self.vpsPathfinder.setGoals(goals: goals.map { $0.asVPSGoal }, callback: completion)
+        serialDispatch.async { self.vpsPathfinder.setGoals(goals: goals.map { $0.asVPSGoal }, callback: completion) }
     }
 
     public func remove(goal: Goal, completion: @escaping () -> Void) {
-        self.vpsPathfinder.removeGoal(id: goal.id, callback: completion)
+        serialDispatch.async { self.vpsPathfinder.removeGoal(id: goal.id, callback: completion) }
     }
 
     public func remove(goals: [Goal], completion: @escaping () -> Void) {
-        self.vpsPathfinder.removeGoals(ids: goals.map { $0.id }, callback: completion)
+        serialDispatch.async { self.vpsPathfinder.removeGoals(ids: goals.map { $0.id }, callback: completion) }
     }
 
     public func popGoal() {
-        self.vpsPathfinder.popGoal()
+        serialDispatch.async { self.vpsPathfinder.popGoal() }
     }
 
     public func forceRefresh(withTSP: Bool, overridePosition: CGPoint?, completion: @escaping () -> Void) {
-        self.vpsPathfinder.forceRefresh(withTSP: withTSP, overridePosition: overridePosition?.asPointF, callback: completion)
+        serialDispatch.async { self.vpsPathfinder.forceRefresh(withTSP: withTSP, overridePosition: overridePosition?.asPointF, callback: completion) }
     }
 }
 
@@ -71,15 +72,15 @@ extension VPSPathfinderAdapter: IPathfinderListener {
     public var id_: String { "VPSPathfinderAdapter" }
 
     public func onCurrentGoalUpdated(goal: IPathfinderGoal?) {
-        currentGoalUpdatedPublisher.send( goal?.asGoal)
+        DispatchQueue.main.async { self.currentGoalUpdatedPublisher.send( goal?.asGoal) }
     }
 
     public func onPathUpdated(path: IPathfinderPath) {
-        pathUpdatedPublisher.send(path.asPath)
+        DispatchQueue.main.async { self.pathUpdatedPublisher.send(path.asPath) }
     }
 
     public func onSortedGoalUpdated(goals: [IPathfinderGoal]) {
-        sortedGoalUpdatedPublisher.send( goals.map { $0.asGoal })
+        DispatchQueue.main.async { self.sortedGoalUpdatedPublisher.send( goals.map { $0.asGoal }) }
     }
 }
 
