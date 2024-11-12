558807e104b59b01a8ea0215d8045064469a8a6a
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2024-11-12 09:44:43 +0100

    Updated packages

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/virtualstores/ios-foundation.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/virtualstores/ios-sensor-fusion.git", .exact("1.0.2")),
+        //.package(url: "https://github.com/virtualstores/ios-sensor-fusion.git", .exact("1.0.2")),
+        .package(url: "https://github.com/virtualstores/ios-sensor-fusion.git", .branchItem("GPS-to-TT2")),
     ],
     targets: [
         .target(
